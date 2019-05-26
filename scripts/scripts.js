0d7b22dbffc15f28fde6e3eb54dca4b2e25155bb
Noah Talerman <noahtalerman@Noahs-MacBook-Pro.local>
2019-05-26 01:24:46 -0700

    style changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,9 +3,14 @@ buttons = (document.getElementsByClassName('selection-btn'));
 
 let userScore = 0;
 let compScore = 0;
+let scoreboard = document.querySelector('.scoreboard');
         
 for (i = 0; i < 3; ++i){
     buttons[i].addEventListener('click', function(){
+
+        //fade out
+        scoreboard.style.opacity = 0;
+
         selectedButton = this.innerHTML;
         computerSelection = computerPlay();
         playRound(selectedButton,computerSelection);
@@ -20,6 +25,7 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
     let announce;
+
     if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
         announce = 'You Win! Rock is wavier than Scissors';
         userScore++;
@@ -49,21 +55,46 @@ function playRound(playerSelection, computerSelection){
     }
     if (userScore === 3){
         announce = 'You win the game! ' + userScore + ' rounds to ' + compScore + '. You\'re ultra wavy';
-        clearBtns();
+        disableBtns();
+        displayRestartBtn();
     }
     if (compScore === 3){
-        announce = 'You lose the game! ' + userScore + ' round(s) to ' + compScore + '. A computer is wavier than you!';
-        clearBtns();
+        announce = 'You lose the game! ' + userScore + ' round(s) to ' + compScore + '. Computer Yeezus is too wavy for you!';
+        disableBtns();
+        displayRestartBtn();
     }
-    document.querySelector('.round-outcome').innerHTML = `${announce}`
+
+    //fade in
+    setTimeout(function(){
+        document.querySelector('.round-outcome').innerHTML = announce;
+        document.querySelector('.user-score').innerHTML = `Your score: ${userScore}`;
+        document.querySelector('.ye-score').innerHTML = `Ye's score: ${compScore}`;
+        scoreboard.style.opacity = 1;
+    },500);
+
     return;
 }
 
-function clearBtns(){
+function disableBtns(){
     let header = document.querySelector('.selection-header');
-    header.style.display = 'none';
+    header.style.opacity = 0.5;
     let buttons = document.getElementsByClassName('selection-btn');
     for (i = 0; i < 3; ++i){
-        buttons[i].style.display = 'none';
+        //change appearance
+        buttons[i].style.opacity = 0.5;
+        buttons[i].style.outline = 'none';
+        buttons[i].style.cursor = 'default';
+        //disable
+        buttons[i].disabled = true;
     }
 }
+
+let restartBtn = document.querySelector('.restart-btn');
+restartBtn.addEventListener('click', function(){
+    location.reload();
+})
+
+function displayRestartBtn(){
+    restartBtn.style.display = 'initial';
+}
+

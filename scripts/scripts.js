279fcf6842b88b6d0436ff96a210cc7b2f8280d4
Noah Talerman <noahtalerman@Noahs-MacBook-Pro.local>
2019-05-28 12:59:12 -0700

    cleaned up code and changed disable buttons so the buttons have no outline when hover

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,75 @@
-let selectedButton;
-buttons = (document.getElementsByClassName('selection-btn'));
-
+//global variable's of user's score and computer's score
 let userScore = 0;
 let compScore = 0;
-let scoreboard = document.querySelector('.scoreboard');
-        
-for (i = 0; i < 3; ++i){
-    buttons[i].addEventListener('click', function(){
 
-        //fade out
-        scoreboard.style.opacity = 0;
+playGame();
 
-        selectedButton = this.innerHTML;
-        computerSelection = computerPlay();
-        playRound(selectedButton,computerSelection);
-    })
+function playGame(){
+    let selectedButton;
+    buttons = (document.getElementsByClassName('selection-btn'));
+            
+    for (i = 0; i < 3; ++i){
+        buttons[i].addEventListener('click', function(){
+
+            //disable buttons
+            disableBtns();
+
+            //fade out scoreboard
+            document.querySelector('.scoreboard').style.opacity = 0;
+
+            //set user's selected move and computer's move
+            selectedButton = this.innerHTML;
+            computerSelection = computerPlay();
+            playRound(selectedButton,computerSelection);
+        });
+    }
 }
 
 function computerPlay(){
+    //selects random play choice for computer
     choiceArray = ["Rock", "Paper", "Scissors"];
     randomChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
     return randomChoice;
 }
 
+function displayCounter(){
+    //countdown from 3
+    let timeLeft = 3;
+    let moveAnnounce;
+    //changes header according to what second the countdown is at
+    let counter = document.querySelector('.counter');
+    counter.style.opacity = 1;
+    let gameTimer = setInterval(function(){
+        if (timeLeft === 3){
+            moveAnnounce = 'Rock';
+        }
+        else if (timeLeft === 2){
+            moveAnnounce = 'Paper';
+        }
+        else if (timeLeft === 1){
+            moveAnnounce = 'Scissors';
+        }
+        counter.innerHTML = moveAnnounce;
+        timeLeft -= 1;
+        if(timeLeft <= -1){
+            clearInterval(gameTimer);
+            counter.innerHTML = 'Shoot';
+        }
+    }, 1000);
+    //fade's out counter after 5 seconds
+    setTimeout(function(){
+        counter.style.opacity = 0;
+        counter.innerHTML = '';
+        enableBtns();
+    }, 5000);
+    return;
+}
+
 function playRound(playerSelection, computerSelection){
     let announce;
+
+    //show and then hide counter
+    displayCounter();
 
     if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
         announce = 'You Win! Rock is wavier than Scissors';
@@ -42,35 +87,45 @@ function playRound(playerSelection, computerSelection){
         announce = 'You Win! Scissors is wavier than Paper';
         userScore++;
     }
-    else if (playerSelection === 'Paper' &&computerSelection === 'Scissors'){
+    else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
         announce = 'You Lose! Paper is less wavy compared to Scissors';
         compScore++;
     }
-    else if (playerSelection === 'Paper' &&computerSelection === 'Rock'){
+    else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
         announce = 'You Win! Paper is wavier than Rock';
         userScore++;
     }
     else if (playerSelection === computerSelection){
         announce = 'Tie! ' + playerSelection + ' and ' + computerSelection + '. Equally wavy';
     }
+
+    //end game when user's or computer's score reaches 3
     if (userScore === 3){
         announce = 'You win the game! ' + userScore + ' rounds to ' + compScore + '. You\'re ultra wavy';
-        disableBtns();
-        displayRestartBtn();
+
+        // disable buttons and display button for game restart after 5 seconds (when timer is complete and scoreboard is visible)
+        setTimeout(function(){
+            disableBtns();
+            displayNewGameBtn();
+        },5000)
     }
     if (compScore === 3){
         announce = 'You lose the game! ' + userScore + ' round(s) to ' + compScore + '. Computer Yeezus is too wavy for you!';
-        disableBtns();
-        displayRestartBtn();
+
+        // disable buttons and display button for game restart after 5 seconds (when timer is complete and scoreboard is visible)
+        setTimeout(function(){
+            disableBtns();
+            displayNewGameBtn();
+        },5000)
     }
 
-    //fade in
+    //fade in scoreboard after 5 seconds
     setTimeout(function(){
         document.querySelector('.round-outcome').innerHTML = announce;
         document.querySelector('.user-score').innerHTML = `Your score: ${userScore}`;
         document.querySelector('.ye-score').innerHTML = `Ye's score: ${compScore}`;
-        scoreboard.style.opacity = 1;
-    },500);
+        document.querySelector('.scoreboard').style.opacity = 1;
+    },5000);
 
     return;
 }
@@ -82,19 +137,35 @@ function disableBtns(){
     for (i = 0; i < 3; ++i){
         //change appearance
         buttons[i].style.opacity = 0.5;
-        buttons[i].style.outline = 'none';
         buttons[i].style.cursor = 'default';
         //disable
         buttons[i].disabled = true;
     }
+    return;
 }
 
-let restartBtn = document.querySelector('.restart-btn');
-restartBtn.addEventListener('click', function(){
-    location.reload();
-})
+function enableBtns(){
+    let header = document.querySelector('.selection-header');
+    header.style.opacity = 1;
+    let buttons = document.getElementsByClassName('selection-btn');
+    for (i = 0; i < 3; ++i){
+        //change appearance
+        buttons[i].style.opacity = 1;
+        buttons[i].style.cursor = 'pointer';
+        //enable
+        buttons[i].disabled = false;
+    }
+    return;
+}
 
-function displayRestartBtn(){
+function displayNewGameBtn(){
+    let restartBtn = document.querySelector('.restart-btn');
+    //reload page when new game button is clicked
+    restartBtn.addEventListener('click', function(){
+    location.reload();
+    });
+    //new game button's initial style = display:hidden
     restartBtn.style.display = 'initial';
+    return;
 }
 
